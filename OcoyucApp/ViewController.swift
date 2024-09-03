9210c848b55d9771c2e6bdef890ad3ee6587aa7a
AntonietaRd1 <a01737276@tec.mx>
2024-09-03 13:10:00 -0600

    Initial Commit

---
+++ OcoyucApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OcoyucApp
+//
+//  Created by Administrador on 03/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
