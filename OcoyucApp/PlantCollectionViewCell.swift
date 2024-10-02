b0cd685a23dc02e3f7f98285e80ba02618b48506
AntonietaRd1 <a01737276@tec.mx>
2024-10-02 13:47:18 -0600

    Cambios en pantalla de catalogo y pantalla de usuario

---
+++ OcoyucApp/PlantCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PlantCollectionViewCell.swift
+//  OcoyucApp
+//
+//  Created by Administrador on 02/10/24.
+//
+
+import UIKit
+
+class PlantCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+}
--- OcoyucApp/ViewController.swift
+++ OcoyucApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //let plantNames =  ["Hortensia", "Rosa", "Gardenia",
+      //                 "clavel", "Tomate", "Tulipan",
+        //               "Violeta", "Orquidea", "Girasol", "Nardos"]
+    
+    //@IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //collectionView.dataSource = self
     }
-
-
 }
 
+//extension ViewController: UICollectionViewDataSource{
+  //  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+ //   }
+    
+   // func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    //}
+    
+//}
