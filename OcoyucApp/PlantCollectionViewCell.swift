4ad8c15ad40e380b5d7faaad63ae56eeaac35a91
AntonietaRd1 <a01737276@tec.mx>
2024-10-08 13:03:06 -0600

    Pantalla de galeria

--- OcoyucApp/PlantCollectionViewCell.swift
+++ OcoyucApp/PlantCollectionViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PlantCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var nameLbl: UILabel!
     
 }
--- OcoyucApp/PlantCollectionViewController.swift
+++ OcoyucApp/PlantCollectionViewController.swift
@@ -5,4 +5,34 @@
 //  Created by Administrador on 02/10/24.
 //
 
-import Foundation
+import UIKit
+
+class PlantCollectionViewController: UIViewController {
+    
+    let plantNames = ["Hortensia", "Rosa", "Gardenia",
+                      "Clavel", "Tomate", "Tulipan",
+                      "Violeta", "Orquidea", "Girasol", "Nardos"]
+    
+    @IBOutlet weak var collectionVIew: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionVIew.dataSource = self
+    }
+}
+
+extension PlantCollectionViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return plantNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "plantCell", for: indexPath) as! PlantCollectionViewCell
+        //cell.backgroundColor = UIColor.blue
+        cell.imageView.image = UIImage(named: "fotoprueba")
+        cell.nameLbl.text = plantNames [indexPath.row]
+        return cell
+    }
+    
+    
+}
